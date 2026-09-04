f25dfd0cf26123a337192875931564034f49f85c
Epxec <trytrykolang11@gmail.com>
2026-09-04 18:18:35 +0800

    chore: add signatures

--- patches/src/main/kotlin/app/epxec/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/epxec/patches/shared/Constants.kt
@@ -11,10 +11,14 @@ object Constants {
         packageName = "com.notch.touch", 
         apkFileType = ApkFileType.XAPK, 
         appIconColor = 0xFF0045,
+        signatures = setOf(
+            "5011d7f93478422063d4b69b524494ec407445ba20b03b728f553b7a5e2d19be"
+        ),
         targets = listOf(
             AppTarget(
                 version = "2.1.7"
             )
+            
         )
     )
 
@@ -23,6 +27,9 @@ object Constants {
         packageName = "com.apktools.app.decompile",
         apkFileType = ApkFileType.XAPK,
         appIconColor = 0xFF1234,
+        signatures = setOf(
+            "f0af89ca6e9e415fa5bdbab307814e09c0788455b09748b8ba57899b18e6471a"
+        ),
         targets = listOf(
             AppTarget(
                 version = "3.3.2",
