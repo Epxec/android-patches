6b57d963494b2375cecd15e90a7ea0a1c850813c
Epxec <trytrykolang11@gmail.com>
2026-09-04 18:53:55 +0800

    feat: add "Remindio" patch

--- patches/src/main/kotlin/app/epxec/patches/shared/Constants.kt
+++ patches/src/main/kotlin/app/epxec/patches/shared/Constants.kt
@@ -25,13 +25,23 @@ object Constants {
         packageName = "com.apktools.app.decompile",
         apkFileType = ApkFileType.XAPK,
         appIconColor = 0xFF1234,
-        signatures = setOf(
-            "f0af89ca6e9e415fa5bdbab307814e09c0788455b09748b8ba57899b18e6471a"
-        ),
         targets = listOf(
             AppTarget(
                 version = "3.3.2",
                 versionCode = 332
+            )
+        )
+    )
+
+    val COMPATIBILITY_Remindio = Compatibility(
+        name = "Remindio",
+        packageName = "dmytro.palamarchuk.dailyreminder",
+        apkFileType = ApkFileType.XAPK,
+        appIconColor = 0xFF1234,
+        targets = listOf(
+            AppTarget(
+                version = "2.25.8",
+                versionCode = 181
             )
         )
     )
