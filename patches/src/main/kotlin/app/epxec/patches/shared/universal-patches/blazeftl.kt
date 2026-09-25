c0e324f58abd96800c8b532134969d9114f08cf1
Epxec <trytrykolang11@gmail.com>
2026-09-25 20:52:04 +0800

    fix: format BLAZEFTL URL for better readability

--- patches/src/main/kotlin/app/epxec/patches/shared/universal-patches/blazeftl.kt
+++ patches/src/main/kotlin/app/epxec/patches/shared/universal-patches/blazeftl.kt
@@ -2,7 +2,9 @@ package app.epxec.patches.shared.blazeftl
 
 import app.epxec.morphe.universal.getUniversalPatch
 
-private const val BLAZEFTL = "https://github.com/BlazeFTL/FTL-Patches/releases/download/v1.43.1/patches-1.43.1.mpp"
+private const val BLAZEFTL =
+    "https://github.com/BlazeFTL/FTL-Patches/releases/download/" +
+        "v1.43.1/patches-1.43.1.mpp"
 
 val spoofAppSignature = getUniversalPatch(
     mppUrl = BLAZEFTL,
